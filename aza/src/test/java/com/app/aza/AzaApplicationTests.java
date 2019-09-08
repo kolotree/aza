d8f7a3b2a1ffc3fb223ca73145b818b5ab4d0332
Milica Travica <mica97@email.com>
2019-09-08 12:22:30 +0200

    Inicijalizacija projekta

---
+++ aza/src/test/java/com/app/aza/AzaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.app.aza;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AzaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
